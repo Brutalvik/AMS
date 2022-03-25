53a89966dd7d66a5efc68e5b3f9dcac32269b2a2
Brutalvik <66142815+Brutalvik@users.noreply.github.com>
2022-03-25 17:49:59 -0400

    Project Initialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from "./App.module.css";
 
-function App() {
+//Components
+import Home from "./Home/Home";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.main}>
+      <header>h1 test</header>
+      <div className={classes.body}>
+        <Home />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
