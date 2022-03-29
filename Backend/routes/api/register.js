13481b431e64317e5a3dbb293e48afadb32909e3
Brutalvik <66142815+Brutalvik@users.noreply.github.com>
2022-03-29 00:57:49 -0400

    Added bcrypt for hashing passwords

--- Backend/routes/api/register.js
+++ Backend/routes/api/register.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const uuid = require("uuid");
+const bcrypt = require("bcrypt");
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+  //Hash Password
+  const saltedPassword = await bcrypt
+    .hash(req.body.password, 10)
+    .then((hash) => {
+      return hash;
+    });
+
   const user = {
     id: uuid.v4(),
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: saltedPassword,
     dob: req.body.dob,
     status: "active",
     isAuthenticated: false,
@@ -17,8 +25,6 @@ router.post("/", (req, res) => {
   if (!user.name || !user.email || !user.password || !user.dob) {
     return res.status(400).json({ error: "Value cannot be blank" });
   }
-
-  //send to database
 
   //get response from database
   res.json({ user: user, message: "Registration Successful" });
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,7 +20,7 @@ const Home = () => {
       </div>
       <div className={classes.center_bar}></div>
       <div className={classes.box}>
-        {status.status === 200 ? <Status /> : <Login />}
+        {status.status === 200 ? <Status /> : <Register />}
       </div>
     </div>
   );
